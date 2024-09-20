cc9ad5c1fff1799636433a9bdcab09652ea2be05
psj0919 <darkjyuk@gmail.com>
2024-09-20 18:11:50 +0900

    Initial commit

---
+++ Config/config.py
@@ -0,0 +1,64 @@
+def dataset_info(dataset_name='vehicledata'):
+    if dataset_name == 'vehicledata':
+        train_path = "/storage/sjpark/vehicle_data/Dataset/train_image/"
+        ann_path = "/storage/sjpark/vehicle_data/Dataset/ann_train/"
+        val_path = '/storage/sjpark/vehicle_data/Dataset/val_image/'
+        val_ann_path = '/storage/sjpark/vehicle_data/Dataset/ann_val/'
+        test_path = '/storage/sjpark/vehicle_data/Dataset/test_image/'
+        test_ann_path = '/storage/sjpark/vehicle_data/Dataset/ann_test/'
+        json_file = '/storage/sjpark/vehicle_data/Dataset/json_file/'
+        num_class = 21
+    else:
+        raise NotImplementedError("Not Implemented dataset name")
+
+    return dataset_name, train_path, ann_path, val_path, val_ann_path, test_path, test_ann_path, num_class
+
+
+def get_config_dict():
+    dataset_name = "vehicledata"
+    name, img_path, ann_path, val_path, val_ann_path, test_path, test_ann_path, num_class, = dataset_info(dataset_name)
+
+
+    dataset = dict(
+        name=name,
+        img_path=img_path,
+        ann_path=ann_path,
+        val_path=val_path,
+        val_ann_path=val_ann_path,
+        test_path=test_path,
+        test_ann_path=test_ann_path,
+        num_class=num_class,
+        image_size = 256,
+        size= (256, 256)
+    )
+    args = dict(
+        gpu_id='1',
+        batch_size=8,
+        network_name="fcn8",
+        epochs=200,
+        num_workers=6
+    )
+    solver = dict(
+        optimizer="adam",
+        scheduler='cycliclr',
+        step_size=5,
+        gamma=0.95,
+        loss="crossentropy",
+        lr=1e-4,
+        weight_decay=5e-4,
+        print_freq=20,
+    )
+    model = dict(
+        resume='/storage/sjpark/vehicle_data/checkpoints/FCN8/256/fcn_epochs:200_optimizer:adam_lr:0.0001_modelfcn8.pth',  # weight_file
+        mode='test',
+        save_dir='/storage/sjpark/vehicle_data/runs/FCN/test/fcn8/256/test',
+        checkpoint='/storage/sjpark/vehicle_data/checkpoints/FCN8/512'  # checkpoint_path
+    )
+    config = dict(
+        args=args,
+        dataset=dataset,
+        solver=solver,
+        model=model
+    )
+
+    return config
---
+++ train.py
@@ -0,0 +1,12 @@
+
+from Config.config import get_config_dict
+from Core.engine_fcn import Trainer
+
+
+
+
+
+if __name__=='__main__':
+    cfg = get_config_dict()
+    trainer = Trainer(cfg)
+    trainer.training()
