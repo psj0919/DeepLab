5b6aad493dc368e07a87dce4264d795d53934594
sjpark <darkjyuk@naver.com>
2025-06-09 16:05:42 +0000

    .

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -8,6 +8,9 @@ from PIL import Image
 import torch.nn.functional as F
 from torchvision import transforms
 from torchvision.transforms import InterpolationMode
+# from gamma_correction import gamma_correction
+from dataset.Adaptive_Gamma_Correction import adaptive_gamma_correction
+from torchvision.transforms import Normalize
 
 
 def resize_train(image, size):
@@ -18,6 +21,68 @@ def resize_train(image, size):
 def resize_label(image, size):
     resize = transforms.Resize(size, interpolation=InterpolationMode.NEAREST)
     return resize(image)
+
+def histogram_equal(img):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
+    l, a, b = cv2.split(img)
+
+    lab_clahe = cv2.merge((cv2.equalizeHist(l), a, b))
+    result = cv2.cvtColor(lab_clahe, cv2.COLOR_YCrCb2RGB)
+
+    return result
+
+def clahe(img):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
+    l, a, b = cv2.split(img)
+
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    l_clahe = clahe.apply(l)
+
+    lab_clahe = cv2.merge((l_clahe, a, b))
+    result = cv2.cvtColor(lab_clahe, cv2.COLOR_YCrCb2RGB)
+
+    return result
+
+def retinex_MSRCR(img, sigma_list=[5, 15, 30], gain=1.0, offset=0):
+    # Dataset : sigma_list = [5, 15, 30], gain=1.0, offset=0
+    # Dataset3 : sigma_list = [15, 80, 250], gain=1.0, offset=0
+    img = img.astype(np.float32) + 1.0
+    log_R = np.zeros_like(img)
+
+    for sigma in sigma_list:
+        blur = cv2.GaussianBlur(img, (0, 0), sigma)
+        log_R += np.log(img) - np.log(blur + 1.0)
+
+    log_R /= len(sigma_list)
+
+    sum_channels = np.sum(img, axis=2, keepdims=True)
+    crf = np.log(img / (sum_channels + 1e-6) + 1.0)
+
+
+    msrcr = gain * log_R * crf + offset
+
+    msrcr = np.clip(msrcr, 0, None)
+    msrcr = cv2.normalize(msrcr, None, 0, 255, cv2.NORM_MINMAX)
+    msrcr = np.clip(msrcr, 0, 255).astype(np.uint8)
+
+    return msrcr
+
+def retinex_MSR(img, sigma_list= [5, 15, 30]):
+    # Dataset : sigma_list = [5, 15, 30]
+    # Dataset3 : sigma_list = [15, 80, 250]
+    img = img.astype(np.float32) + 1.0
+    result = np.zeros_like(img)
+    for sigma in sigma_list:
+        blur = cv2.GaussianBlur(img, (0, 0), sigma)
+        result += np.log(img) - np.log(blur + 1)
+    result = result / len(sigma_list)
+    result = cv2.normalize(result, None, 0, 255, cv2.NORM_MINMAX)
+    return np.uint8(result)
+
+
+def gammacorrection(img, gamma=0.5):
+    table = np.array([((i / 255.0) ** gamma) * 255 for i in np.arange(256)]).astype("uint8")
+    return cv2.LUT(img, table)
 
 
 class vehicledata():
@@ -58,8 +123,8 @@ class vehicledata():
 
         img = np.array(img, dtype=np.uint8)
         label = np.array(label, dtype=np.uint8)
-        img, label = self.transform(img, label)
 
+        img, label = self.transform(img, label)
 
         # create one-hot encoding
         h, w = label.size()
@@ -72,10 +137,15 @@ class vehicledata():
 
     def transform(self, img, label):
         img = img[:, :, ::-1]  # RGB -> BGR
+
+        img = clahe(img)
+
         img = img.astype(np.float64)
         img = img.transpose(2, 0, 1)
         img = torch.from_numpy(img).float() / 255.0    # when image is into network -> image pixel value is between 0 and 1
         label = torch.from_numpy(label).float()
+        normalize = Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        img = normalize(img)
 
         return img, label
 
@@ -90,8 +160,8 @@ class vehicledata():
 
 
 if __name__ == "__main__":
-    image_path = "/storage/sjpark/vehicle_data/Dataset/train_image/"
-    annotation_path = "/storage/sjpark/vehicle_data/Dataset/ann_train/"
+    image_path = "/storage/sjpark/vehicle_data/Dataset3/train_image/"
+    annotation_path = "/storage/sjpark/vehicle_data/Dataset3/ann_train/"
     dataset_object = vehicledata(image_path, annotation_path, 21, (256, 256))
 
-    img, target, label,  index = dataset_object.__getitem__(0)
+    img, target, label,  index = dataset_object.__getitem__(20)
